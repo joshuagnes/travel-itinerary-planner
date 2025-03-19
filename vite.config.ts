f5d0044e42265c09fcecd5ceec5757120b9d14c4
Joshua <cesteraime@gmail.com>
2025-03-18 22:39:33 -0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/DEV209_FinalProject",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
