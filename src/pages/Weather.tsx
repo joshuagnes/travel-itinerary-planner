5701614fd698c67f13e684b7f210e676cc9cf904
Joshua <cesteraime@gmail.com>
2025-03-19 22:10:06 -0700

    change .gitignore

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -64,7 +64,7 @@ const Weather: React.FC = () => {
   const fetchWeather = async (city: string): Promise<WeatherData | null> => {
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
-        import.meta.env.APP_ID
+        import.meta.env.VITE_APP_ID
       }`;
       const response = await fetch(url);
       const data = await response.json();
